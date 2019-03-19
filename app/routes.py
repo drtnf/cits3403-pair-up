3d2e9a0ee72d90f4a8fb5dfce668d1ed56a66aa7
drtnf <tim.french@uwa.edu.au>
2019-03-19 13:20:06 +0800

    built very simple web rendering with jinga2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Pair Up!"
+    projects = [{
+            'id':'00',
+            'name':'Pair up!',
+            'team':'Tim and Miguel',
+            'demonstration':{
+                'location':'CSSE:2.01',
+                'datetime':'Wednesday March 20, 2pm'
+                }
+            }]
+    return render_template('index.html', projects=projects)
