fff06b43c4c124875e68944cc79509f1dcbd3c6d
drtnf <tim.french@uwa.edu.au>
2019-04-08 17:15:48 +0800

    major refactor, and database linked in.

--- app/forms.py
+++ app/forms.py
@@ -1,20 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired, regexp
+from wtforms.validators import DataRequired, regexp, EqualTo
+from app.models import Lab
 
+'''class to hold elements and validators for login form'''
 class LoginForm(FlaskForm):
-    studentNumber = StringField('Student Number', validators=[DataRequired(), regexp('^\d{8}$')])
-    pinCode = PasswordField('Pin Code', validators=[regexp('^\d{4}$')])
-    remember_me = BooleanField('Remember Me')
-    submit =SubmitField('Sign In')
+  student_number = StringField('Student Number', validators=[DataRequired(), regexp('^\d{8}$',message='must by 8 digits')])
+  pin = PasswordField('Pin Code', validators=[regexp('^\d{4}$',message='must be 4 digits')])
+  remember_me = BooleanField('Remember Me')
+  submit =SubmitField('Sign In')
 
-class projectForm(FlaskForm):
-    cits3403Days = [(1,'Monday May 20, 2-4, CSSE:2.01'),(2,'Wednesday May 22, 2-4, ELEC:1.51'),(3,'Wednesday May 24, 2-4, CSSE:2.01')]
-    cits5505Days = [(4,'Wednesday May 22, 12-2, CSSE:2.03')]
-    cits3403Times = [(1,'4.00-4.05')]
-    partnerNumber = StringField('Student Number', validators=[DataRequired()])
-    projectDescription = StringField('Project Description', validators=[DataRequired()])
-    demonstrationTute = SelectField('Laboratory', choices=cits3403Days)
-    demonstrationTime = SelectField('Time', choices=cits3403Times)
-    #put in dynamic update here
+'''class to hold elements and validators for registration form'''
+class RegistrationForm(FlaskForm):
+  student_number = StringField('Student Number', validators=[DataRequired(), regexp('^\d{8}$', message='must be 8 digits')])
+  prefered_name = StringField('Prefered Name', validators=[])
+  pin =PasswordField('Current Pin',validators=[regexp('^\d{4}$',message='must be four digits')],default='0000')
+  new_pin = PasswordField('New Pin', validators=[regexp('^\d{4}$', message='must be four digits')])
+  new_pin2 = PasswordField('Confirm Pin', validators=[EqualTo('new_pin')])
+  submit = SubmitField('Sign up')
+
+
+def getLabs():
+    labs = Lab.get_available_labs()
+    choices = []
+    for l in labs:
+      choices.append((str(l.lab_id), l.lab+' '+str(l.time))) 
+    return choices
+
+'''Class to hold form elements for project registration and editting'''
+class ProjectForm(FlaskForm):
+  partner_number = StringField('Student Number', validators=[DataRequired(),regexp('^\d{8}$')],default='00000000')
+  project_description = StringField('Project Description', validators=[DataRequired()])
+  lab = SelectField('Demonstration Laboratory', choices = getLabs())
+  submit = SubmitField('Submit project')
 
--- app/models.py
+++ app/models.py
@@ -1,31 +1,102 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class Student(db.Model):
-    number = db.Column(db.String(8), primary_key = True)
-    firstName = db.Column(db.String(64))
-    surname = db.Column(db.String(64))
-    cits3403 = db.Column(db.Boolean)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.pid'),nullable=True)
+#allows login to get student from database, given id
+#will be stored as current_user?
+@login.user_loader
+def load_student(id):
+  return Student.query.get(int(id))
 
-    def __repr__(self):
-        return '[Number:{}, Name:{}, CITS3403:{}]'.format(number, self.str(), cits34403)
+#student database will be prepopulated with 
+# student numbers, firstname, surname, CITS3403 boolean
+#students can add/edit pin and project id
+class Student(UserMixin, db.Model):
+  __tablename__='students'
+  id = db.Column(db.String(8), primary_key = True)#prepopulate
+  first_name = db.Column(db.String(64))#prepopulate
+  surname = db.Column(db.String(64))#prepopulate
+  prefered_name = db.Column(db.String(64))#defaults to first_name if empty
+  cits3403 = db.Column(db.Boolean)#prepopulate
+  password_hash = db.Column(db.String(128))#overkill to hash a four digit pin, but included for learning.
+  project_id = db.Column(db.Integer, db.ForeignKey('projects.project_id'),nullable=True) #assigned when project registered
+
+  def registered(number):
+    student = Student.query.filter_by(number = id.data).first()
+    if student.password_hash is not None:
+        raise validationError('Student is already registered')
+    return True
+
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+  def is_committed(self):
+    return self.project_id is not None
+
+  def __repr__(self):
+    return '[Number:{}, Name:{}, CITS3403:{}]'.format(self.number, \
+      self.str(), \
+      self.cits34403)
     
-    def __str__(self):
-        return firstName+' '+surname
+  def __str__(self):
+    return self.first_name+' '+self.surname
 
+#Projects are created editted by students
 class Project(db.Model):
-    pid = db.Column(db.Integer, primary_key = True)
-    team = db.relationship('Student', backref='project', lazy='dynamic')
-    description = db.Column(db.String(64))
-    demoLab = db.Column(db.String(64))
-    demoTime = db.Column(db.DateTime)
+  __tablename__='projects'
+  project_id = db.Column(db.Integer, primary_key = True)
+  description = db.Column(db.String(64))
+  lab_id = db.Column(db.Integer,db.ForeignKey('labs.lab_id'),nullable=True)
 
-    def __repr__(self):
-      return '[PID:{}, Team:{}, Desc:{},Lab:{},Time:{}]'.format(pid,team,description,demoLab,demoTime.str())
+  def __repr__(self):
+    return '[PID:{}, Desc:{},LabId:{}]'.format(\
+        self.project_id,\
+        self.description,\
+        self.lab_id)
    
-    def __str__(self):
-      return 'Project {}: {}'.format(pid,description)
+  def __str__(self):
+    return 'Project {}: {}'.format(self.project_id,self.description)
 
+  '''returns a list of students involved in the project'''
+  def get_team(self):
+    return Student.query.filter_by(project_id=self.project_id).all()
+
+  def get_lab(self):
+    lab = Lab.query.filter_by(project_id=self.project_id)\
+        .add_columns(Lab.lab,Lab.time).first()
+    return lab
+
+    
+#demonstration lab times are prepopulated and intended to be immutable
+class Lab(db.Model):
+  __tablename__='labs'
+  lab_id = db.Column(db.Integer, primary_key=True)
+  lab = db.Column(db.String(64))
+  time = db.Column(db.String(64))
+
+  def get_project(self):
+    return Project.query.filter_by(lab_id=self.lab_id).first()
+
+  def is_available(self):
+    return self.get_project() is None
+
+  def get_available_labs():
+    labs = Lab.query.\
+        outerjoin(Project, Lab.lab_id==Project.lab_id).\
+        filter(Project.project_id==None).all()
+    return labs
+
+  def __repr__(self):
+    return '[LID:{}, Lab:{}, time:{}]'.format( \
+        self.lab_id, \
+        self.lab, \
+        self.time)
+   
+  def __str__(self):
+    return 'Lab {}: {}'.format(self.lab,self.time)
 
 
 
