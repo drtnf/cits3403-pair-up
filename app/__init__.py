dc0628282068fc2010bdd8251932e9a40d8c518e
drtnf <tim.french@uwa.edu.au>
2020-04-20 12:49:31 +0800

    Updated vulnerable packages

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from config import DevelopmentConfig
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig())
+app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
--- config.py
+++ config.py
@@ -1,19 +1,21 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir,'.env'))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'sshh!'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  DEBUG=True
 
 class ProductionConfig(Config):
   ENV='production'
-  SECRET_KEY = os.environ.get('SECRET_KEY')
-#  SQLALCHEMY_DATABASE_URI = Postgres remote 
+  SECRET_KEY = os.environ['SECRET_KEY']
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] 
 
 class DevelopmentConfig(Config):
-  ENV='development'
+  FLASK_ENV='development'
   DEBUG=True
 
 class TestingConfig(Config):
